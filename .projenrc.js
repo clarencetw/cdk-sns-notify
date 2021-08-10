b38147f9d4e13968aa41c70696065f2a455a9b2f
Clarence <mr.lin.clarence@gmail.com>
2021-08-11 00:20:17 +0800

    Update projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,25 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism } = require('projen');
+
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
-  authorAddress: "mr.lin.clarence@gmail.com",
-  authorName: "Clarence",
-  cdkVersion: "1.72.0",
-  name: "cdk-sns-notify",
-  repository: "https://github.com/clarencetw/cdk-sns-notify.git",
+  authorAddress: 'mr.lin.clarence@gmail.com',
+  authorName: 'Clarence',
+  cdkVersion: '1.72.0',
+  name: 'cdk-sns-notify',
+  repository: 'https://github.com/clarencetw/cdk-sns-notify.git',
   keywords: ['aws', 'cdk', 'line notify'],
-  dependabot: false,
+  defaultReleaseBranch: 'master',
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['clarencetw'],
+  },
   catalog: {
     twitter: 'Clarence_Lin',
     announce: false,
@@ -16,24 +28,12 @@ const project = new AwsCdkConstructLibrary({
     distName: 'cdk-sns-notify',
     module: 'cdk_sns_notify',
   },
-  deps: [
-    "@aws-cdk/core",
-    "@aws-cdk/aws-iam",
-    "@aws-cdk/aws-lambda",
-    "@aws-cdk/aws-sns-subscriptions",
-    "@aws-cdk/aws-lambda-nodejs",
-    '@aws-cdk/aws-ec2',
-    '@aws-cdk/aws-sns',
-    '@aws-cdk/aws-cloudwatch',
-    '@aws-cdk/aws-cloudwatch-actions',
+  cdkDependencies: [
+    '@aws-cdk/core',
+    '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-lambda',
+    '@aws-cdk/aws-sns-subscriptions',
     '@aws-cdk/aws-lambda-nodejs',
-  ],
-  peerDeps: [
-    "@aws-cdk/core",
-    "@aws-cdk/aws-iam",
-    "@aws-cdk/aws-lambda",
-    "@aws-cdk/aws-sns-subscriptions",
-    "@aws-cdk/aws-lambda-nodejs",
     '@aws-cdk/aws-ec2',
     '@aws-cdk/aws-sns',
     '@aws-cdk/aws-cloudwatch',
