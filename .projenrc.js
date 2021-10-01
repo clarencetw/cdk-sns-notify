9258f38e7065b71b87fef7153d47b34e06aa2747
Clarence <mr.lin.clarence@gmail.com>
2021-10-01 09:09:31 +0800

    chore: set projen minNodeVersion

--- .projenrc.js
+++ .projenrc.js
@@ -10,6 +10,7 @@ const project = new AwsCdkConstructLibrary({
   repository: 'https://github.com/clarencetw/cdk-sns-notify.git',
   keywords: ['aws', 'cdk', 'line notify'],
   defaultReleaseBranch: 'master',
+  minNodeVersion: '14.18.0',
   depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
